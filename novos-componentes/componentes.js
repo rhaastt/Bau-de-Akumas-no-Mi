3ddd6c87320f0ea6cb12f2e329f4aaef1c5ad2dc
Luis Miguel <luismiguel.vieira17@gmail.com>
2025-09-28 21:10:49 -0300

    responsividade ?

---
+++ novos-componentes/componentes.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {    
+    document.getElementById("");
+    document.getElementById("");
+    document.getElementById("");
+    document.getElementById("");
+    document.getElementById("");
+    document.getElementById("");
+    document.getElementById("");
+    document.getElementById("");
+});
